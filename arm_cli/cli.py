4ec26ce356847bdc7895b6ed2db72bcca5791763
Matthew Powelson <powelson.matthew@gmail.com>
2025-03-12 10:38:06 -0400

    Add self update to help install from source

--- arm_cli/cli.py
+++ arm_cli/cli.py
@@ -1,6 +1,8 @@
 import click
-from arm_cli.container import container
+from arm_cli.container.container import container
+from arm_cli.self.self import self
 from arm_cli.system.system import system
+
 
 @click.group()
 @click.version_option()
@@ -8,8 +10,9 @@ def cli():
     """Experimental CLI for deploying robotic applications"""
     pass
 
-# Add the container management commands as a sub-group
+# Add command groups
 cli.add_command(container)
+cli.add_command(self)
 cli.add_command(system)
 
 if __name__ == "__main__":
