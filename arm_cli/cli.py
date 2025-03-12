0cecc0b50a5cb7d48b9b7c545e05fe24bbdd0fb8
Matthew Powelson <powelson.matthew@gmail.com>
2025-03-12 15:54:22 -0400

    Allow -h in addition to --help

--- arm_cli/cli.py
+++ arm_cli/cli.py
@@ -4,11 +4,12 @@ from arm_cli.self.self import self
 from arm_cli.system.system import system
 
 
-@click.group()
 @click.version_option()
+@click.group(context_settings=dict(help_option_names=["-h", "--help"]))
 def cli():
     """Experimental CLI for deploying robotic applications"""
     pass
+
 
 # Add command groups
 cli.add_command(container)
