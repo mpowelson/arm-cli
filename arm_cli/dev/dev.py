bffcc944168d206f44335171109244823008ebf7
Matthew Powelson <powelson.matthew@gmail.com>
2026-01-08 15:13:56 -0500

    Remove alias module

--- arm_cli/dev/dev.py
+++ arm_cli/dev/dev.py
@@ -1,11 +1,9 @@
 import click
 
 # Import subgroups
-import arm_cli.dev.alias.alias
 import arm_cli.dev.env.env
 
 # Get subgroup objects
-alias = arm_cli.dev.alias.alias.alias
 env = arm_cli.dev.env.env.env
 
 
@@ -17,4 +15,3 @@ def dev():
 
 # Register subgroups
 dev.add_command(env)
-dev.add_command(alias)
